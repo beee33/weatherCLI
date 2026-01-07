d41bf82267fd99a51cd06a4e4efcfde5919cac31
beee33 <jjcbee33@gmail.com>
2026-01-07 18:21:40 -0500

    made it possible to check depenencies before running

--- install-non-root.sh
+++ install-non-root.sh
@@ -31,62 +31,79 @@ comp_bin() {
         done
 
 
-        check_command_git_install=$(command -v git)
-        check_command_python3_install=$(command -v python3) 
-	check_command_venv_install=$(python3 -m venv -h | grep "positional arguments:")
+	check_command_git_install=$(command -v git)
+	check_command_python3_install=$(command -v python3) 
+	#check_command_venv_install=$(python3 -m venv -h | grep "positional arguments:")
 
-        if [ -n "$check_command_git_install" ] 
-        then
-                if [ -n "$check_command_python3_install" ] 
-                then
-			if [ -n "$check_command_venv_install" ]
+	if [ -n "$check_command_git_install" ] 
+	then
+		if [ -n "$check_command_python3_install" ] 
+		then
+
+
+			if [ $is_version = "1" ]
 			then
+				echo "cloning weatherCLI";
+				git clone https://github.com/beee33/weatherCLI
 
-
-				if [ $is_version = "1" ]
-				then
-					echo "cloning weatherCLI";
-					git clone https://github.com/beee33/weatherCLI
-
-					cd weatherCLI
-				else
-					echo "cloning latest version:";
-					download_link=$(curl -s https://api.github.com/repos/beee33/weatherCLI/releases/latest | grep "tarball_url" | cut -d '"' -f 4)
-					echo $download_link;
-					curl -L $download_link -o latest.tar.gz
-
-					tar -xf latest.tar.gz
-
-					file_to_goto=$(ls | grep weatherCLI)
-					cd $file_to_goto
-
-				fi
-
-				echo "creating virtual environment";
-				python3 -m venv venv
-
-				echo "entering virtual environment";
-				source venv/bin/activate
-
-				echo "installing dependencys";
-				python3 -m pip install -r requirements.txt
-
-				echo "installing pyinstaller"
-				python3 -m pip install pyinstaller
-
-				echo "compiling";
-				pyinstaller main.py --onefile
-
-				mv dist/main weatherCLI
-
-				mkdir ~/.local/bin/weather-machine/
-				cp weatherCLI ~/.local/bin/weather-machine/weatherCLI
-				chmod +x ~/.local/bin/weather-machine/weatherCLI
+				cd weatherCLI
 			else
-				echo "python venv not installed";
+				echo "cloning latest version:";
+				download_link=$(curl -s https://api.github.com/repos/beee33/weatherCLI/releases/latest | grep "tarball_url" | cut -d '"' -f 4)
+				echo $download_link;
+				curl -L $download_link -o latest.tar.gz
+
+				tar -xf latest.tar.gz
+
+				file_to_goto=$(ls | grep weatherCLI)
+				cd $file_to_goto
+
+			fi
+
+			echo "creating virtual environment";
+			python3 -m venv venv
+
+			venv_is_works=$(ls venv | grep "No such file or directory")
+
+			if [ -n "$venv_is_works" ] 
+			then
+				echo "venv not installed";
 				exit
 
 			fi
+			
+			env
+
+			echo "entering virtual environment";
+			source venv/bin/activate
+
+			pip_installed=$(python3 -m pip -V | grep "No module named pip")
+
+			if [ -n "$pip_installed" ] 
+			then 
+				echo "pip not installed";
+				exit
+			fi
+
+			echo "installing dependencys";
+			python3 -m pip install -r requirements.txt
+
+			echo "installing pyinstaller"
+			python3 -m pip install pyinstaller
+
+			echo "compiling";
+			pyinstaller main.py --onefile
+
+			mv dist/main weatherCLI
+
+			mkdir ~/.local/bin/weather-machine/
+			cp weatherCLI ~/.local/bin/weather-machine/weatherCLI
+			chmod +x ~/.local/bin/weather-machine/weatherCLI
+
+
+
+
+
 
 
                 else
